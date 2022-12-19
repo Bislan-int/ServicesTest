39ddea2833c8b2f18fc9ac59f8f4cee8f2e41d53
Bislan <mamaliev.bislan@mail.ru>
2022-12-19 11:31:58 +0300

    JobScheduler. Метод enqueue

--- app/src/main/java/com/example/servicestest/MyJobService.kt
+++ app/src/main/java/com/example/servicestest/MyJobService.kt
@@ -2,6 +2,8 @@ package com.example.servicestest
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -14,19 +16,28 @@ class MyJobService : JobService() {
         log("onCreate")
     }
 
-    override fun onStartJob(p0: JobParameters?): Boolean {
-        log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(p0, true)
         }
         return true
     }
 
-    override fun onStopJob(p0: JobParameters?): Boolean {
+    override fun onStopJob(params: JobParameters?): Boolean {
         log("onStopJob")
         return true
     }
@@ -44,5 +55,12 @@ class MyJobService : JobService() {
     companion object {
 
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
